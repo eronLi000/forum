9dae7cf4b0473d553653d074148a55f7fa21bdf4
eronLi000 <lyg19960729@gmail.com>
2020-12-20 23:44:35 +0800

    add cookie checking

--- src/main/java/life/yg/forum/mapper/UserMapper.java
+++ src/main/java/life/yg/forum/mapper/UserMapper.java
@@ -3,6 +3,8 @@ package life.yg.forum.mapper;
 import life.yg.forum.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 /**
  * created by yigang on 2020/12/20
@@ -13,4 +15,7 @@ public interface UserMapper {
             "(name,account_id,token,gmt_create,gmt_modified) " +
             "values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
     void insert(User user);
+
+    @Select("select * from user where token=#{token}")
+    User findByToken(@Param("token") String token);
 }
